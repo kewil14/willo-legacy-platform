2d6f866f459b95e37fe55416b0f738bc4897cbbf
NtalByte <wilfried@connecttechnology.fr>
2025-02-06 00:46:11 +0100

    config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php 
+define("DB_SERVER", "localhost");
+define("DB_NAME", "willo");
+define ("DB_USER", "root");
+define ("DB_PASSWORD", "");
+
+define ("DB_TABLE", "visits");
+define ("IMG", "./1px.png"); // change this constant to use dif. image/path
+?>
